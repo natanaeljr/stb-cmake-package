1a4828e0cf649fb873c7b5f45b5d22d411a44ae4
Natanael J Rabello <natanaeljrabello@gmail.com>
2022-01-08 20:00:22 -0300

    v0.9.0: Add stb_truetype.h and stb_rect_pack.h
    Signed-off-by: Natanael J Rabello <natanaeljrabello@gmail.com>

---
+++ src/stb_rect_pack.c
@@ -0,0 +1,3 @@
+// Create stb rect-pack implementation
+#define STB_RECT_PACK_IMPLEMENTATION
+#include "stb/stb_rect_pack.h"
---
+++ src/stb_truetype.c
@@ -0,0 +1,3 @@
+// Create stb truetype implementation
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "stb/stb_truetype.h"
